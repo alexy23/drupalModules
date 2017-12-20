f12d1af24c440622ce37e4f745f26b8b41b2169e
alexy23 <alex.busin123@gmail.com>
2017-12-20 14:22:04 +0600

    fixed bugs in module discount_code

--- web/modules/custom/discount_code/src/Entity/DiscountCode.php
+++ web/modules/custom/discount_code/src/Entity/DiscountCode.php
@@ -1,76 +1,48 @@
 <?php
 
+/**
+ * @file
+ * Contains \Drupal\discount_code\Entity\DiscountCode.
+ */
+
 namespace Drupal\discount_code\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\user\Entity\User;
+
 /**
- * Defines the discount_code entity.
+ * Defines the DiscountCode entity.
  *
+ * @ingroup discount_code
  *
  * @ContentEntityType(
  *   id = "discount_code",
- *   label = @Translation("Discount Code"),
+ *   label = @Translation("Discount code"),
  *   base_table = "discount_code",
  *   entity_keys = {
  *     "id" = "id",
- *     "uuid" = "uuid"
+ *     "uuid" = "uuid",
  *     "uid" = "uid",
  *     "code" = "code",
  *   },
  * )
  */
-
-/**
- * Class DiscountCode.
- *
- * @package Drupal\discount_code\Entity
- */
 class DiscountCode extends ContentEntityBase implements ContentEntityInterface {
-  /**
-   * Extends class baseFieldDefinition and create enitity discount_code.
-   * @param EntityTypeInterface $entity_type
-   * @return array|\Drupal\Core\Field\FieldDefinitionInterface[]
-   */
-
-  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
-
-    $fields = parent::baseFieldDefinitions($entity_type);
-
-    $fields['id'] = BaseFieldDefinition::create('integer')
-      ->setLabel(t('ID'))
-      ->setReadOnly(TRUE);
-
-    $fields['uuid'] = BaseFieldDefinition::create('uuid')
-      ->setDescription(t('The user UUID.'));
-
-    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
-      ->setLabel(t('UID'))
-      ->setDescription(t('The UID of the  reference user id entity.'))
-      ->setReadOnly(TRUE)
-      ->setSetting('target_type', 'user')
-      ->setSetting('handler', 'default');
-
-    $fields['code'] = BaseFieldDefinition::create('string')
-      ->setLabel(t('Code'))
-      ->setDescription('10-signed string of the user for  promo')
-      ->setSettings(array(
-        'default_value' => '',
-        'max_length' => 16,
-        'text_processing' => 0,
-      ));
-
-    return $fields;
+  public function getUid() {
+    return $this->getEntityKey('uid');
+  }
+  public function getCode() {
+    $test = $this->getEntityKey('code');
+    return $test;
   }
 
-  /**
-   * Generate promo code user after register.
-   *
-   * @return string
-   *   Return string generate code.
-   */
+  public function getOwnerName() {
+    return User::load($this->getUid())->getDisplayName();
+  }
+
   public static function generateDiscountCode() {
     $temp_array_gen = array();
     $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
@@ -83,34 +55,39 @@ class DiscountCode extends ContentEntityBase implements ContentEntityInterface {
   }
 
   /**
-   * Function request field code in entity of database.
-   *
-   * @return mixed
-   *   Return object entity in db.
+   * Determines the schema for the base_table property defined above.
    */
-  //  public function getCode() {
-  //    return $this->getEntityKey('code');
-  //  }
-  //
-  //  /**
-  //   * Function request field Uid in table discount_code of database.
-  //   *
-  //   * @return mixed
-  //   *   Return object entity in db.
-  //   */
-  //  public function getUid() {
-  //    return $this->getEntityKey('uid');
-  //  }
-  //
-  //  /**
-  //   * Function request name of user in entity promo code.
-  //   *
-  //   * @return array|\Drupal\Component\Render\MarkupInterface|mixed|null|string
-  //   *   Return name of object user which relation with discount code.
-  //   */
-  //  public function getOwnerName() {
-  //    return User::load($this->getUid())->getDisplayName();
-  //  }
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
 
+    // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The ID of the Discount Code entity.'))
+      ->setReadOnly(TRUE);
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['uuid'] = BaseFieldDefinition::create('uuid')
+      ->setLabel(t('UUID'))
+      ->setDescription(t('The UUID of the Discount Code entity.'))
+      ->setReadOnly(TRUE);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('UID'))
+      ->setDescription(t('The UID of the  reference user id entity.'))
+      ->setReadOnly(TRUE)
+      ->setSetting('target_type', 'user')
+      ->setSetting('handler', 'default');
+
+    $fields['code'] = BaseFieldDefinition::create('string')
+      ->setLabel(t("The Promo code"))
+      ->setDescription(t('The code of user.'))
+      ->setSettings(array(
+        'default_value' => '',
+        'max_length' => 20,
+        'text_processing' => 0,
+      ));
+
+    return $fields;
+  }
 
 }
